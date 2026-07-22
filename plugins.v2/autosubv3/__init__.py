9c18189b0ad93565853953b900229268f61bd3ee
sokouu87 <160479016+sokouu87@users.noreply.github.com>
2026-07-22 15:50:46 +0800

    翻译prompt加长句意译规则:长句从句按中文语序重组拆分意译,改善直译导致的不通顺;版本3.5.60

--- plugins.v2/autosubv3/__init__.py
+++ plugins.v2/autosubv3/__init__.py
@@ -52,7 +52,7 @@ class AutoSubv3(AutoSubv3CompatMixin, _PluginBase):
     # 主题色
     plugin_color = "#2C4F7E"
     # 插件版本
-    plugin_version = "3.5.59"
+    plugin_version = "3.5.60"
     # 插件作者
     plugin_author = "ifsherlock"
     # 作者主页
